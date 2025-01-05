7428e4f6a3269d2ff57b1ad91c76d5c6f8871ef8
Asif Sayyed <asifdotexe@gmail.com>
2025-01-05 23:37:38 +0530

    #5 updated the referenced variable names from config

--- python/src/data_ingestion.py
+++ python/src/data_ingestion.py
@@ -3,14 +3,14 @@ import kagglehub
 import pandas as pd
 import logging
 
-from src.config import PROJECT_ROOT, DATA_DIR, DATASET_PATH, DATASET_NAME
+from src.config import PROJECT_ROOT, DATA_DIR, KAGGLE_SOURCE_PATH, KAGGLE_DATA_NAME
 
 def ingest_data():
     """Ingests the data from kaggle using kagglehub"""
     # Download latest version of data from kaggle
-    path = kagglehub.dataset_download(DATASET_PATH)
+    path = kagglehub.dataset_download(KAGGLE_SOURCE_PATH)
     # Load the dataset as a pandas dataframe and save it to the raw directory
-    df = pd.read_csv(os.path.join(path,DATASET_NAME))
+    df = pd.read_csv(os.path.join(path,KAGGLE_DATA_NAME))
 
     # standardize the dataset columns
     df.columns = df.columns.str.lower().str.replace(' ', '_')
