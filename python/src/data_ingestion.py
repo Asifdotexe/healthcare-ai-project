7b1fabc202b912b4bd6bda2eb6856c9e8852f46a
Asif Sayyed <asifdotexe@gmail.com>
2025-01-05 23:32:17 +0530

    #5 decentralized the data ingestion into a script for scalability

---
+++ python/src/data_ingestion.py
@@ -0,0 +1,20 @@
+import os
+import kagglehub
+import pandas as pd
+import logging
+
+from src.config import PROJECT_ROOT, DATA_DIR, DATASET_PATH, DATASET_NAME
+
+def ingest_data():
+    """Ingests the data from kaggle using kagglehub"""
+    # Download latest version of data from kaggle
+    path = kagglehub.dataset_download(DATASET_PATH)
+    # Load the dataset as a pandas dataframe and save it to the raw directory
+    df = pd.read_csv(os.path.join(path,DATASET_NAME))
+
+    # standardize the dataset columns
+    df.columns = df.columns.str.lower().str.replace(' ', '_')
+    data_save_path = os.path.join(DATA_DIR, 'raw', "chest_xray_vectorized_raw.csv")
+    df.to_csv(data_save_path, index=False)
+    
+    print(f"✅ | Dataset ingested and updated please check {data_save_path}")
