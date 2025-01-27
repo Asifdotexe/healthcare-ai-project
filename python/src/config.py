6bd19fff6029b72efd38f8d705ac9f2eac683c29
Asif Sayyed <asifdotexe@gmail.com>
2025-01-27 07:13:21 +0530

    #7 updated the configuration file

--- python/src/config.py
+++ python/src/config.py
@@ -9,8 +9,9 @@ DATA_DIR = PROJECT_ROOT / 'data'
 NOTEBOOKS_DIR = PROJECT_ROOT / 'python/notebook'
 SRC_DIR = PROJECT_ROOT / 'python/src'
 
-# for kagglehub usecase and not to be confused with the actual project data file path
-# source fileL src/data_ingestion.py
+# for kagglehub usecase and not to be confused with the actual project data
+# file path
+# source file: src/data_ingestion.py
 KAGGLE_SOURCE_PATH = "redwankarimsony/chestxray8-dataframe"
 KAGGLE_DATA_NAME = "train_df.csv"
 
